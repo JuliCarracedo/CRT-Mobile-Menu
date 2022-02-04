fb811d5190b95dc32a22761c74195ae45b89649e
JuliCarracedo <julicarra@hotmail.com>
2022-02-04 15:59:51 -0300

    Fix linter issues

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-const obj = document.querySelector("button.menu-bars")
-const popup = document.querySelector(".popup-links")
-const cross = document.querySelector(".close-popup")
-const popupLinks = document.querySelectorAll("a.nav-link.bigger")
+const obj = document.querySelector('button.menu-bars');
+const popup = document.querySelector('.popup-links');
+const cross = document.querySelector('.close-popup');
+const popupLinks = document.querySelectorAll('a.nav-link.bigger');
 
-console.log(popupLinks)
 const handleHamburger = (e) => {
-    e.stopPropagation();
-    popup.classList.replace('untoggled','toggled')
+  e.stopPropagation();
+  popup.classList.replace('untoggled', 'toggled');
 
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
 
-    window.onscroll = function() {
-        window.scrollTo(scrollLeft, scrollTop);
-    };
-}
+  window.onscroll = () => {
+    window.scrollTo(scrollLeft, scrollTop);
+  };
+};
 
-obj.addEventListener('click', e =>{ handleHamburger(e); });
-
+obj.addEventListener('click', (e) => { handleHamburger(e); });
 
 const handleClose = (e) => {
-    e.stopPropagation();
-    popup.classList.replace('toggled','untoggled')
+  e.stopPropagation();
+  popup.classList.replace('toggled', 'untoggled');
 
-    window.onscroll = function() {};
-}
+  window.onscroll = () => {};
+};
 
- popupLinks.forEach((link)=>{
-    link.addEventListener('click', e=>{handleClose(e)})
- })
+popupLinks.forEach((link) => {
+  link.addEventListener('click', (e) => { handleClose(e); });
+});
 
-cross.addEventListener('click', e =>{ handleClose(e); });
+cross.addEventListener('click', (e) => { handleClose(e); });
